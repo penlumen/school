79556a27daf966e62750310fe8f178395441822d
tesimune <tesimune@gmail.com>
2026-01-16 09:18:40 +0100

    Update

--- src/controllers/v1/class.controller.ts
+++ src/controllers/v1/class.controller.ts
@@ -14,6 +14,7 @@ export const index: RequestHandler = async (req: Request, res: Response) => {
   const token = req.headers.authorization || null;
   const decoded = verifyToken(token, res);
 
+  console.log(branch_uuid);
   if (!branch_uuid) {
     res.status(400).json({
       status: 400,
--- src/controllers/v1/student.controller.ts
+++ src/controllers/v1/student.controller.ts
@@ -19,6 +19,7 @@ export const index: RequestHandler = async (
   const token = req.headers.authorization || null;
   const decoded = verifyToken(token, res);
 
+  console.log(branch_uuid);
   if (!branch_uuid) {
     res.status(400).json({
       status: 400,
