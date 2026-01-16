b9782d2636f85d320804c247cb2f861333112c55
tesimune <tesimune@gmail.com>
2026-01-16 07:28:04 +0100

    Update

--- src/controllers/v1/result.controller.ts
+++ src/controllers/v1/result.controller.ts
@@ -17,7 +17,7 @@ export const index: RequestHandler = async (
   const status = req.query.status as 'PENDING' | 'APPROVED' | 'REJECTED' | undefined;
   const search = req.query.search as string;
   const page = Number(req.query.page) || 1;
-  const limit = Number(req.query.limit) || 10;
+  const limit = Number(req.query.limit) || 30;
   const skip = (page - 1) * limit;
 
   const token = req.headers.authorization || null;
